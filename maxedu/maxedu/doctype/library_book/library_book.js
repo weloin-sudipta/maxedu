b07770658c5420c23d3e06b6bde74f45e20f36e9
sudipta-weloin <sudipta.ghosh@weloin.com>
2026-03-12 17:54:04 +0530

    fix: library book

--- maxedu/maxedu/doctype/library_book/library_book.js
+++
@@ -1,9 +0,0 @@
-frappe.ui.form.on('Library Book', {
-    refresh(frm) {
-        frm.set_query("tags", function() {
-            return {
-                filters: {}
-            };
-        });
-    }
-});
--- maxedu/maxedu/doctype/library_book/library_book.py
+++ maxedu/maxedu/doctype/library_book/library_book.py
@@ -2,9 +2,4 @@ import frappe
 from frappe.model.document import Document
 
 class LibraryBook(Document):
-
-    def validate(self):
-
-        if self.publication_year:
-            if self.publication_year > frappe.utils.now_datetime().year:
-                frappe.throw("Publication year cannot be in the future")
+   pass
