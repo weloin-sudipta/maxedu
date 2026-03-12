e612e4ea8af4bf92ac66891272510032ddba8b43
weloin-kishalay <kishalay.das@weloin.com>
2026-03-12 12:41:18 +0530

    library member doctype

---
+++ maxedu/maxedu/doctype/library_member/library_member.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, Maxedu and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class LibraryMember(Document):
+	pass
