2a9cc79b8cb161ce35c63440b70b18412920b724
weloin-kishalay <kishalay.das@weloin.com>
2026-03-24 17:20:08 +0530

    some darkmode changes

--- frontend/components/dashbaord/academicCalendar.vue
+++ frontend/components/dashbaord/academicCalendar.vue
@@ -218,8 +218,8 @@ const nextMonth = () => {
 
 const getTypeStyles = (type) => {
   if(type==="exam") return "bg-rose-50 dark:bg-rose-900/20 text-rose-500 dark:text-rose-400"
-  if(type==="holiday") return "bg-amber-50 dark:bg-amber-900/20 text-amber-500 dark:text-amber-400"
-  if(type==="assignment") return "bg-green-50 dark:bg-green-900/20 text-green-500 dark:text-green-400"
+  if(type==="assignment") return "bg-amber-50 dark:bg-amber-900/20 text-amber-500 dark:text-amber-400"
+  if(type==="holiday") return "bg-green-50 dark:bg-green-900/20 text-green-500 dark:text-green-400"
   return "bg-indigo-50 dark:bg-indigo-900/20 text-indigo-500 dark:text-indigo-400"
 }
 
