e58027ad66b41728a2c72cde1632990ef14a0f61
weloin-kishalay <kishalay.das@weloin.com>
2026-03-26 13:45:27 +0530

    showing students in teacher

--- maxedu/api_folder/student.py
+++ maxedu/api_folder/student.py
@@ -17,6 +17,22 @@ def get_student_group():
 
 @frappe.whitelist(allow_guest=True)
 def get_student_by_institute():
-   return frappe.get_all("Student",fields="*")
+    teacher_name = frappe.db.get_value("User", frappe.session.user, "full_name")
+    institute_name = frappe.db.get_value(
+        "Instructor",
+        {"instructor_name": teacher_name},
+        "institute"
+    )
+    students = frappe.get_all("Student", filters={"institute": institute_name}, fields=["name"])
+    
+    # Extract list of student names from the array
+    student_names = [s["name"] for s in students]
+    
+    programmes = frappe.get_all(
+        "Program Enrollment",
+        filters={"student": ["in", student_names]},
+        fields="*"
+    )
+    return programmes
    
     
